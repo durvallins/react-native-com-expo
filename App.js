eb3b9a5c9b6eac2797de9fcbc19b8a9c05055bc4
durvallins <durval.lsiqueira@gmail.com>
2022-04-29 23:07:41 -0300

    unimed

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Nova linha</Text>
+      <Text>Nova linha unimed</Text>
       <StatusBar style="auto" />
     </View>
   );
